b4a65f9af407861a0675e45e06e6d57b7be44577
Siripiri-hue <daemonprocellae@live.it>
2022-07-18 16:48:41 +0200

    set first elements

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+
+    render() {
+        return (
+            <div className='myNavbar col-2'>
+                Questa è la navbar
+            </div>
+        )
+    }
+}
+
+export default Navbar;
---
+++ src/components/page.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Page extends Component {
+
+    render() {
+        return (
+            <div className='myPage col flex-grow-1'>
+                <h1>This is the rest of the page</h1>
+            </div>
+        )
+    }
+}
+
+export default Page;
